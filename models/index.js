088bd504919f180d1ac273f077364a96293748ad
KevinKiely <Kevinkiely762@gmail.com>
2024-05-31 12:39:41 -0400

    File structure, installing dependencies, Creating Models for user and thoughts

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Thought = require('./thought');
+const User = require('./user');
+const reactionSchema = require('./reaction');
+
+module.exports = { Thought, User, reactionSchema };
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema({
+    username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    },
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+
+    },
+    thoughts: [],
+    friends: [],
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
